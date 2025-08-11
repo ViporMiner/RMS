6cf80a5bc9be5219d88faec487962983a569ebe2
ViporMinerProxy <57853730+ViporMiner@users.noreply.github.com>
2025-08-12 00:10:47 +0800

    2.0.3

--- OLD_2/install.sh
+++ OLD_2/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#bash <(curl -s -L https://raw.githubusercontent.com/EvilGenius-dot/RMS/main/install.sh)
-#bash <(curl -s -L -k https://raw.njuu.cf/EvilGenius-dot/RMS/main/install.sh)
-#bash <(curl -s -L -k https://raw.yzuu.cf/EvilGenius-dot/RMS/main/install.sh)
-#bash <(curl -s -L -k https://raw.nuaa.cf/EvilGenius-dot/RMS/main/install.sh)
+#bash <(curl -s -L https://raw.githubusercontent.com/VIPORMiner/RMS/main/install.sh)
+#bash <(curl -s -L -k https://cdn.jsdelivr.net/gh/VIPORMiner/RMS@main/install.sh)
+#bash <(curl -s -L -k http://saz1122.oss-cn-hongkong.aliyuncs.com/ViporMiner/RMS/raw/main/install.sh)
+#bash <(curl -s -L -k https://raw.nuaa.cf/VIPORMiner/RMS/main/install.sh)
 clear
 
 [ $(id -u) != "0" ] && { echo "请使用ROOT用户进行安装, 输入sudo -i切换。"; exit 1; }
